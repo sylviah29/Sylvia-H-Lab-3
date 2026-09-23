21fb1f7106adb229244e45ea7ac1347b65ee2d84
sylviah29 <shabashy29@setoncatholic.org>
2026-09-23 20:14:27 +0000

    Commit 1

---
+++ main.py
@@ -0,0 +1,24 @@
+#this function adds two numbers
+def add(x,y):
+    print(x + y)
+
+#this function subtracts two numbers
+def multiply(x,y):
+    print(x * y)
+
+#this function multiplies two numbers
+def subtract(x,y):
+    print(x - y)
+
+#this function divides two numbers
+def divide(x,y):
+    print(x / y)
+
+
+x= int(input("Enter your first number:"))
+y= int(input("Enter your second number:"))
+
+add(x,y)
+multiply(x,y)
+subtract(x,y)
+divide(x,y)
