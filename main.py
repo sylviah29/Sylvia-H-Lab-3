65a0498053dbc703af4b80fa64ae5d962c4a32c0
sylviah29 <shabashy29@setoncatholic.org>
2026-09-23 20:21:05 +0000

    Commit 2

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def subtract(x,y):
 def divide(x,y):
     print(x / y)
 
+print("Welcome to my calculator")
+print("What would you like to do?")
+print("Type (a)dd (s)ubtract (m)ultiply d(ivide)")
 
 x= int(input("Enter your first number:"))
 y= int(input("Enter your second number:"))
@@ -22,3 +25,7 @@ add(x,y)
 multiply(x,y)
 subtract(x,y)
 divide(x,y)
+
+
+print("Welcone to my calculator")
+print()
